e7a4aa3c7836d9faba2d057a8c9856ecb0837ec3
Rob Sizeland <robcakes69@gmail.com>
2022-10-25 11:02:21 +0100

    Add more else results for checkWinner and got score increment working

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,7 @@ let playerChoice = document.getElementById('player-choice');
 let resultText = document.getElementById('result');
 let player;
 let computer;
-let result;
-let playerScore = document.getElementById('score');
+let playerScore = 0;
 let playerLives;
 
 for (let choice of choices) {
@@ -17,16 +16,18 @@ for (let choice of choices) {
         console.log(player);
         runGame(player);
     });
-}
+};
 
 function runGame(player) {
     playerChoice.textContent = `You chose: ${player}`; 
 
     computerRandomNumber();
-    computerChoice.textContent = `Computer chose: ${computer}`
+    computerChoice.textContent = `Computer chose: ${computer}`;
 
     resultText.textContent = checkWinner();
-}
+
+    document.getElementById('score').innerHTML = playerScore;
+};
 
 function computerRandomNumber() {
     let compRandom = Math.floor(Math.random() * 5) + 1;
@@ -47,21 +48,41 @@ function computerRandomNumber() {
         case 5:
             computer = "Spock";
             break;
-    }
-}
+    };
+};
 
 function checkWinner() {
     if (player == computer) {
-        return "Draw! Go again!"
-    } else {
-        if (player == "Rock") {
-            if (computer == "Scissors") {
-                ++playerScore;
-                return "You Win!"
-            }
-            else if (computer == "Paper") {
-                return "You Lose!"
-            }
+        return "Draw! Go again!";
+    } else if (player == "Rock") {
+        if (computer == "Scissors") {
+            playerScore += 1;
+            return "You Win! Rock crushes Scissors!";
+        }
+        else if (computer == "Paper") {
+            return "You Lose! Paper covers Rock!";
+        }
+        else if (computer == "Lizard") {
+            playerScore += 1;
+            return "You Win! Rock crushes Lizard!";
+        }
+        else if (computer == "Spock") {
+            return "You Lose! Spock vaporises Rock";
+        }
+    } else if (player == "Scissors") {
+        if (computer == "Rock") {
+            return "You Lose! Rock crushes Scissors!";
+        }
+        else if (computer == "Paper") {
+            playerScore += 1;
+            return "You Win! Scissors cuts Paper!";
+        }
+        else if (computer == "Lizard") {
+            playerScore += 1;
+            return "You Win! Scissors decapitates Lizard!";
+        }
+        else if (computer == "Spock") {
+            return "You Lose! Spock smashes Scissors!";
         }
     }
-}
+};
