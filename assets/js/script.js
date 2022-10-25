7ad0e7ea992421e8fbacb6c76c55b8bbcd070b87
Rob Sizeland <robcakes69@gmail.com>
2022-10-25 10:22:29 +0100

    Add checkWinner function and managed to output player and computer choice with a result

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,44 +5,63 @@ let choices = document.getElementsByClassName('choice');
 let computerChoice = document.getElementById('computer-choice');
 let playerChoice = document.getElementById('player-choice');
 let resultText = document.getElementById('result');
-let playerScore;
+let player;
+let computer;
+let result;
+let playerScore = document.getElementById('score');
 let playerLives;
 
 for (let choice of choices) {
     choice.addEventListener('click', function() {
-        playerChoice = this.textContent;
-        console.log(playerChoice);
-        runGame(playerChoice);
+        player = this.innerHTML;
+        console.log(player);
+        runGame(player);
     });
 }
 
-function runGame(playerChoice) {
-    playerChoice.textContent = `Player: ${playerChoice}`; 
+function runGame(player) {
+    playerChoice.textContent = `You chose: ${player}`; 
 
-    computerChoice.textContent = `Computer: ${computerChoice}`
+    computerRandomNumber();
+    computerChoice.textContent = `Computer chose: ${computer}`
 
     resultText.textContent = checkWinner();
 }
 
-function computer() {
+function computerRandomNumber() {
     let compRandom = Math.floor(Math.random() * 5) + 1;
-
+    console.log(compRandom);
     switch(compRandom) {
         case 1:
             computer = "Rock";
             break;
-        case 1:
+        case 2:
             computer = "Paper";
             break;
-        case 1:
+        case 3:
             computer = "Scissors";
             break;
-        case 1:
+        case 4:
             computer = "Lizard";
             break;
-        case 1:
+        case 5:
             computer = "Spock";
             break;
     }
 }
 
+function checkWinner() {
+    if (player == computer) {
+        return "Draw! Go again!"
+    } else {
+        if (player == "Rock") {
+            if (computer == "Scissors") {
+                ++playerScore;
+                return "You Win!"
+            }
+            else if (computer == "Paper") {
+                return "You Lose!"
+            }
+        }
+    }
+}
